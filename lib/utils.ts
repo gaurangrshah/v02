078e48bd15b0a42adb8ce89f87423944748b6d15
gshah2020 <gaurang.r.shah@gmail.com>
2024-02-17 09:47:54 -0500

    chore: add truncate utility helper

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,9 +1,24 @@
-import { customAlphabet } from "nanoid";
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx';
+import { customAlphabet } from 'nanoid';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
-export const nanoid = customAlphabet("abcdefghijklmnopqrstuvwxyz0123456789");
+export const nanoid = customAlphabet('abcdefghijklmnopqrstuvwxyz0123456789');
+
+/* -------------------------------------------------------------------------- */
+/*                                STRING UTILS                                */
+/* -------------------------------------------------------------------------- */
+/**
+ * truncate a string to a certain length
+ *
+ * @export
+ * @param {string} str
+ * @param {number} n
+ * @return {*}
+ */
+export function truncate(str: string, n: number) {
+  return str?.length > n ? str.substring(0, n - 1) + '...' : str;
+}
