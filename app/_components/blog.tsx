a5aff3b656940d8cff859a3bc884aceada5ba49d
gshah2020 <gaurang.r.shah@gmail.com>
2024-02-17 09:48:08 -0500

    feat: fetch blog from rss feed

--- app/_components/blog.tsx
+++ app/_components/blog.tsx
@@ -1,6 +1,12 @@
+"use server";
+
 import Image from 'next/image';
 
+import { z } from 'zod';
+
 import { ScrollArea } from '@/components/ui/scroll-area';
+
+import { truncate } from '@/lib/utils';
 
 const ARTICLES = [
   {
@@ -68,7 +74,34 @@ const ARTICLES = [
   },
 ].reverse();
 
-export function Blog() {
+const articlesSchema = z.object({ title: z.string(), thumbnail: z.string(), link: z.string(), pubDate: z.string(), description: z.string() });
+
+type Article = z.infer<typeof articlesSchema>;
+
+export async function Blog() {
+
+  const response = await fetch('https://api.rss2json.com/v1/api.json?rss_url=https://blog.gshahdev.com/rss.xml')
+  const data = await response.json()
+  const articles = data.items.map((item: Article) => {
+    if (articlesSchema.parse(item)) {
+      return {
+        title: truncate(item.title, 36),
+        thumbnail: item.thumbnail,
+        link: item.link,
+        pubDate: item.pubDate,
+        description: truncate(item.description.replace(/(<([^>]+)>)/gi, ""), 145)
+      }
+    } else return null;
+  });
+
+  function getThumbnails(article: Article) {
+    return ARTICLES.filter((a) => a.url === article.link).map((a) => a.image)[0];
+  }
+
+
+
+
+
   return (
     <section
       id="blog"
@@ -83,7 +116,7 @@ export function Blog() {
       <div>
         <ScrollArea className="h-[30rem] w-full rounded-md px-3 pt-4">
           <ul className="group/list">
-            {ARTICLES.map((article, i) => (
+            {articles?.length ? articles.map((article: { title: string; thumbnail: string; link: string; pubDate: string; description: string; }, i: number) => (
               <li key={i} className="mb-12">
                 <div className="group relative grid grid-cols-8 gap-4 transition-all sm:items-center sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50 px-3">
                   <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-emerald-800/10 dark:lg:group-hover:bg-emerald-800/20 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg" />
@@ -96,20 +129,20 @@ export function Blog() {
                     data-img={1}
                     className="z-10 col-span-2 rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:col-span-2"
                     style={{ color: "transparent" }}
-                    src={article.image}
+                    src={getThumbnails(article)}
                   />
                   <div className="z-10 col-span-6">
                     <h3 className="-mt-1">
                       <a
                         className="inline-flex items-baseline font-medium leading-tight text-accent dark:hover:text-teal-300 dark:focus-visible:text-teal-300 group/link"
-                        href={article.url}
+                        href={article.link}
                         target="_blank"
                         rel="noreferrer noopener"
                         aria-label={`${article.title} (opens in a new tab)`}
                       >
                         <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block" />
                         <span>
-                          {article.title}{" "}
+                          {truncate(article.title, 36)}{" "}
                           <span className="inline-block">
                             <svg
                               xmlns="http://www.w3.org/2000/svg"
@@ -128,14 +161,15 @@ export function Blog() {
                         </span>
                       </a>
                     </h3>
-                    {/* <p className="-mt-1 text-xs text-slate-400 leading-6">{article.date}</p> */}
-                    <p className="mt-2 text-xs leading-normal text-pretty">
-                      {article.description}
+                    <p className="mt-2 text-xs leading-normal text-pretty truncate text-ellipsis">
+                      {truncate(article.description.replace(/(<([^>]+)>)/gi, ""), 145)}
                     </p>
                   </div>
                 </div>
               </li>
-            ))}
+            )) : null}
+            {/* <p className="-mt-1 text-xs text-slate-400 leading-6">{article.date}</p> */}
+            {/* <p className="-mt-1 text-xs text-slate-400 leading-6">{article.pubDate}</p> */}
           </ul>
         </ScrollArea>
       </div>
