f6dca89b0629c9fb28bd9f713eb65f8146d9de7a
gshah2020 <gaurang.r.shah@gmail.com>
2024-02-17 21:50:41 -0500

    chore: update env_vars for production

--- app/robots.ts
+++ app/robots.ts
@@ -9,6 +9,9 @@ export default function robots(): MetadataRoute.Robots {
         disallow: ['/dashboard', '/account', '/settings'],
       },
     ],
-    sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
+    sitemap:
+      process.env.NODE_ENV === 'development'
+        ? `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`
+        : `${process.env.NEXT_PUBLIC_VERCEL_URL}/sitemap.xml`,
   };
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,10 @@ import { MetadataRoute } from 'next';
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   return [
     {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/`,
+      url:
+        process.env.NODE_ENV === 'development'
+          ? `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`
+          : `${process.env.NEXT_PUBLIC_VERCEL_URL}/sitemap.xml`,
     },
   ];
 }
