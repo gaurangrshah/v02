baa8f35c173a2d0576692561c51a39d99ac93377
gshah2020 <gaurang.r.shah@gmail.com>
2024-02-17 23:10:11 -0500

    fix: vercel url for google-search console

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,7 +6,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       url:
         process.env.NODE_ENV === 'development'
           ? `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`
-          : `${process.env.NEXT_PUBLIC_VERCEL_URL}/sitemap.xml`,
+          : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/sitemap.xml`,
     },
   ];
 }
