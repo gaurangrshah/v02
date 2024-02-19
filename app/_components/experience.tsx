7cee76d6ae5d5990b4f013ba2719a4fa4f11e4c7
gshah2020 <gaurang.r.shah@gmail.com>
2024-02-19 01:01:49 -0500

    chore: remove unused conditional

--- app/_components/experience.tsx
+++ app/_components/experience.tsx
@@ -63,7 +63,7 @@ export function Experience() {
           {JOB_EXPERIENCE.map((job) => (
             <li key={job.id} className="mb-12">
               <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
-                <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-emerald-800/10 dark:lg:group-hover:bg-emerald-800/20 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg" />
+                <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-emerald-800/5 dark:lg:group-hover:bg-emerald-800/20 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg" />
                 <header
                   className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2"
                   aria-label={job.date}
@@ -72,40 +72,14 @@ export function Experience() {
                 </header>
                 <div className="z-10 sm:col-span-6">
                   <h3 className="font-medium leading-snug text-slate-200">
-                    <div>
-                      {job.company.link ? <a
-                        className="inline-flex items-baseline font-medium leading-tight text-accent dark:hover:text-teal-300 dark:focus-visible:text-teal-300 group/link"
-                        href={`/${job.company.link}`}
-                        target="_blank"
-                        rel="noreferrer noopener"
-                        aria-label={`${job.title} at ${job.company} (opens in a new tab)`}
-                      >
-                        <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block" />
-                        <span>
-                          {job.title} ·{" "}
-                          <span className="inline-block">
-                            {job.company}
-                            {/* <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              viewBox="0 0 20 20"
-                              fill="currentColor"
-                              className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
-                              aria-hidden="true"
-                            >
-                              <path d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" />
-                            </svg> */}
-                          </span>
+                    <div className='text-accent dark:hover:text-teal-300 dark:focus-visible:text-teal-300 group/link'>
+                      <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block group/link" />
+                      <span>
+                        {job.title} ·{" "}
+                        <span className="inline-block">
+                          {job.company}
                         </span>
-                      </a> : (
-                        <>
-                          <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block group/link" />
-                          <span>
-                            {job.title} ·{" "}
-                            <span className="inline-block">
-                              {job.company}
-                            </span>
-                          </span></>
-                      )}
+                      </span>
                     </div>
                   </h3>
                   <p className="mt-2 text-sm text-slate-500 hover:text-white leading-normal">
