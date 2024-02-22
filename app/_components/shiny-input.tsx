c063e4093aab0d485677200b3808961830149868
gshah2020 <gaurang.r.shah@gmail.com>
2024-02-21 21:35:02 -0500

    chore: make shiny-input extensible

--- app/_components/shiny-input.tsx
+++ app/_components/shiny-input.tsx
@@ -11,10 +11,14 @@ import { cn } from '@/lib/utils';
 export type ShinyInputProps = {
   field: ComponentProps<'input'>;
   className: string;
-  children: React.ReactNode;
+  label: {
+    className?: ComponentProps<'label'>['className'];
+    text: string | undefined;
+  }
+  children?: React.ReactNode;
 }
 
-export function ShinyInput({ field, className, children }: ShinyInputProps) {
+export function ShinyInput({ field, className, label, children }: ShinyInputProps) {
   const divRef = useRef<HTMLInputElement>(null);
   const [isFocused, setIsFocused] = useState(false);
   const [position, setPosition] = useState({ x: 0, y: 0 });
@@ -47,8 +51,8 @@ export function ShinyInput({ field, className, children }: ShinyInputProps) {
     setOpacity(0);
   };
   return (
-    <div className="relative">
-      <label htmlFor="search" className="text-xs font-medium text-gray-900 sr-only dark:text-white">Email</label>
+    <div className="relative group" role="group">
+      <label htmlFor={field.name} className={cn(label.className, "peer text-xs font-medium text-gray-900 sr-only dark:text-white capitalize")}>{label.text || field.name}</label>
       <input onMouseMove={handleMouseMove}
         onFocus={handleFocus}
         onBlur={handleBlur}
@@ -56,19 +60,19 @@ export function ShinyInput({ field, className, children }: ShinyInputProps) {
         onMouseLeave={handleMouseLeave}
         autoComplete="off"
         {...field}
-        className={cn(className, "ps-12 w-full border border-neutral-800 bg-background focus-visible:bg-neutral-950 text-slate-400 transition-colors duration-500 placeholder:select-none placeholder:text-slate-600 focus:border-[hsl(150_40%_52%)] focus:outline-none focus-visible:focus:ring-0 focus-visible:ring-0 focus-within:focus:ring-0 rounded-md")} />
+        className={cn(className, " peer-focus-within:-translate-y-4 peer w-full border border-neutral-800 bg-background focus-visible:bg-neutral-950 text-slate-400 transition-all duration-500 placeholder:select-none placeholder:text-slate-600 focus:border-[hsl(150_40%_52%)] focus:outline-none focus-visible:focus:ring-0 focus-visible:ring-0 focus-within:focus:ring-0 rounded-md")} />
       <input
         ref={divRef}
         disabled
         style={{
           border: "1px solid hsl(var(--secondary))",
           opacity,
-          WebkitMaskImage: `radial-gradient(30% 30px at ${position.x}px ${position.y}px, black 45%, transparent)`,
+          WebkitMaskImage: `radial-gradient(30% 28px at ${position.x}px ${position.y}px, black 45%, transparent)`,
         }}
         aria-hidden="true"
-        className="border-[hsl(var)] pointer-events-none absolute left-0 top-0 z-10 h-11 w-full cursor-default rounded-md border bg-transparent p-3.5 opacity-0 transition-opacity duration-500 placeholder:select-none"
+        className="peer peer-focus-within:hidden visible border-[hsl(var)] pointer-events-none absolute left-0 top-0 z-10 h-11 w-full cursor-default rounded-md border bg-transparent p-3.5 opacity-0 transition-opacity duration-500 placeholder:select-none"
       />
       {children}
-    </div >
+    </div>
   )
 }
