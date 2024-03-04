1fbf6418d35f10b0256062b2fc77020f469b54ba
gshah2020 <gaurang.r.shah@gmail.com>
2024-03-04 17:21:48 -0500

    chore: format layout + fix: root providers structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,22 +4,27 @@ import type { Metadata } from 'next';
 import { GeistSans } from 'geist/font/sans';
 
 import RootProviders from './root-providers';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://gshahdev.com/'),
   title: {
-    default: "G.Shah Dev",
-    template: "%s | G.Shah Dev | Freelance Web Developer & UX Engineer | NJ Consulting",
+    default: 'G.Shah Dev',
+    template:
+      '%s | G.Shah Dev | Freelance Web Developer & UX Engineer | NJ Consulting',
   },
-  description: "Help brands scale their web presence with freelance web development, UX design, growth marketing, branding & tech consulting",
-  keywords: "Technology Consultant, Freelance Developer, Web Developer, Web Designer, Web Development, Search Engine Optimization, UX Design, Clifton, NJ, New Jersey, E-commerce Website Development, WordPress Development, Landing Page Design, Shopify Development, Hoboken, Montclair, Parsippany.",
+  description:
+    'Help brands scale their web presence with freelance web development, UX design, growth marketing, branding & tech consulting',
+  keywords:
+    'Technology Consultant, Freelance Developer, Web Developer, Web Designer, Web Development, Search Engine Optimization, UX Design, Clifton, NJ, New Jersey, E-commerce Website Development, WordPress Development, Landing Page Design, Shopify Development, Hoboken, Montclair, Parsippany.',
   twitter: {
-    site: "@soham_asmi",
+    site: '@soham_asmi',
     card: 'summary_large_image',
   },
   openGraph: {
-    title: "G.Shah Dev | Freelance Web Developer & UX Engineer | NJ Consulting",
-    description: "Help brands scale their web presence with freelance web development, UX design, growth marketing, branding & tech consulting",
+    title: 'G.Shah Dev | Freelance Web Developer & UX Engineer | NJ Consulting',
+    description:
+      'Help brands scale their web presence with freelance web development, UX design, growth marketing, branding & tech consulting',
     type: 'website',
     locale: 'en_US',
     url: 'https://gshahdev.com',
@@ -32,11 +37,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={GeistSans.className} suppressHydrationWarning>
-        <RootProviders>
-          {children}
-        </RootProviders>
+        <RootProviders>{children}</RootProviders>
       </body>
     </html>
   );
--- app/root-providers.tsx
+++ app/root-providers.tsx
@@ -4,24 +4,21 @@ import { ThemeProvider } from '@/components/ThemeProvider';
 import { Toaster } from '@/components/ui/sonner';
 import AnalyticsComponent from '@/components/analytics';
 import { Suspense } from 'react';
-import { CookieConsentBanner } from '@/components/analytics/cookie-consent';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import Fallback from '@/components/fallback';
 
-export default function RootProviders({ children }: { children: React.ReactNode }) {
-
+export default function RootProviders({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        <Suspense fallback={<>{children}</>}>
-          <AnalyticsComponent>
-            {children}
-          </AnalyticsComponent>
-        </Suspense>
+      <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+        <AnalyticsComponent>{children}</AnalyticsComponent>
       </ThemeProvider>
-      <Analytics />
-      <CookieConsentBanner />
       <Toaster richColors />
       <SpeedInsights />
     </>
-  )
+  );
 }
